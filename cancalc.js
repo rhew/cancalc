8d36118e270f59ae7940995ed355e4c2bee76163
James Rhew <james@rhew.org>
2013-11-29 00:09:36 -0500

    Pass in radius

--- cancalc.js
+++ cancalc.js
@@ -1,19 +1,20 @@
-function canstructionDiameter(count)
+function canstructionDiameter(radius, count)
 {
-    STANDARD_303_CAN_RADIUS_INCHES = 1.59375;
-    STANDARD_300_CAN_RADIUS_INCHES = 1.5;
-    var radius = STANDARD_300_CAN_RADIUS_INCHES;
+    var centerToCanCenter = Math.sin((Math.PI - 2 * Math.PI / count ) / 2)
+        * 2 * radius / Math.sin(2 * Math.PI / count);
 
-    return 2 * (Math.sin((Math.PI - 2 * Math.PI / count ) / 2)
-        * 2 * radius / Math.sin(2 * Math.PI / count) + radius);
+    return 2 * (centerToCanCenter + radius);
 }
 
 function buildCanTable()
 {
+    STANDARD_303_CAN_RADIUS_INCHES = 1.59375;
+    STANDARD_300_CAN_RADIUS_INCHES = 1.5;
+
     var canData = [];
 
     for (var i = 3; i<=100; i++) {
-        canData.push([i, canstructionDiameter(i).toFixed(1)]);
+        canData.push([i, canstructionDiameter(STANDARD_300_CAN_RADIUS_INCHES, i).toFixed(1)]);
     }
 
     $(function() {
