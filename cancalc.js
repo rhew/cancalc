a5b5e0e6752ee755a9cdc5a5d7ccbdf9a3e8851b
James Rhew <james@rhew.org>
2013-11-29 00:36:51 -0500

    Convert can type to diameter
    @see http://www.cancentral.com/beverage-cans/standards

--- cancalc.js
+++ cancalc.js
@@ -1,5 +1,14 @@
-function canstructionDiameter(radius, count)
+function canTypeToInches(canType)
 {
+    var inches = Number(canType.substring(0,1));
+    var sixteenths = Number(canType.substring(1, 2));
+    return inches + sixteenths / 16;
+}
+
+function canstructionDiameter(canType, count)
+{
+    radius = canTypeToInches(canType) / 2;
+
     var centerToCanCenter = Math.sin((Math.PI - 2 * Math.PI / count ) / 2)
         * 2 * radius / Math.sin(2 * Math.PI / count);
 
@@ -8,13 +17,11 @@ function canstructionDiameter(radius, count)
 
 function buildCanTable()
 {
-    STANDARD_303_CAN_RADIUS_INCHES = 1.59375;
-    STANDARD_300_CAN_RADIUS_INCHES = 1.5;
-
+    var canType = '300';
     var canData = [];
 
     for (var i = 3; i<=100; i++) {
-        canData.push([i, canstructionDiameter(STANDARD_300_CAN_RADIUS_INCHES, i).toFixed(1)]);
+        canData.push([i, canstructionDiameter(canType, i).toFixed(1)]);
     }
 
     $(function() {
